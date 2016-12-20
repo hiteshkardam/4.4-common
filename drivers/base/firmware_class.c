3c44cf44580d9855b66fd1a72bada2bd4767fcfe
Christopher R. Palmer <crpalmer@gmail.com>
2016-12-20 06:47:10 -0500

    pme: Import HTC's firmware loading hacks
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -275,12 +275,14 @@ static void fw_free_buf(struct firmware_buf *buf)
 
 /* direct firmware loading support */
 static char fw_path_para[256];
-static const char * const fw_path[] = {
+static char *fw_path[32] = {
 	fw_path_para,
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
-	"/lib/firmware"
+	"/lib/firmware",
+	"/firmware/image"
+        ,"/firmware/cradio"
 };
 
 /*
@@ -290,6 +292,8 @@ static const char * const fw_path[] = {
  */
 module_param_string(path, fw_path_para, sizeof(fw_path_para), 0644);
 MODULE_PARM_DESC(path, "customized firmware image search path with a higher priority than default path");
+
+module_param_array(fw_path, charp, NULL, 0644);
 
 static int fw_read_file_contents(struct file *file, struct firmware_buf *fw_buf)
 {
@@ -327,17 +331,36 @@ static int fw_get_filesystem_firmware(struct device *device,
 {
 	int i, len;
 	int rc = -ENOENT;
-	char *path;
-
-	path = __getname();
+	char *path = __getname();
+	const char* radio_image_select_path = "/dev/block/bootdevice/by-name/fsc";
+	const int fsc_offset = 532; 
+	static char radio_image_info[4] = "";
+	static bool is_first = true;
+	struct file *fsc_file;
+        
 	if (!path)
 		return -ENOMEM;
 
+        
+        if (!strncmp( buf->fw_id, "modem", 5)) {
+          if ( is_first ) {
+            fsc_file = filp_open( radio_image_select_path, O_RDONLY, 0);
+            if ( !IS_ERR(fsc_file) ) {
+              kernel_read( fsc_file, fsc_offset, radio_image_info, 4*sizeof(char) );
+              filp_close( fsc_file, NULL );
+            }
+            else {
+              dev_err(device,"firmware: fsc_file open fail, err value = %d\n", IS_ERR(fsc_file));
+            }
+            is_first = false;
+          }
+        }
+        
 	for (i = 0; i < ARRAY_SIZE(fw_path); i++) {
 		struct file *file;
 
-		/* skip the unset customized path */
-		if (!fw_path[i][0])
+		
+		if (!fw_path[i] || !fw_path[i][0])
 			continue;
 
 		len = snprintf(path, PATH_MAX, "%s/%s",
@@ -346,7 +369,13 @@ static int fw_get_filesystem_firmware(struct device *device,
 			rc = -ENAMETOOLONG;
 			break;
 		}
-
+                
+				if ( !strcmp( radio_image_info, "CDMA" ) && !strcmp( fw_path[i], "/firmware/image" )
+                  && ( !strncmp( buf->fw_id, "modem", 5 ) || !strncmp( buf->fw_id, "mba", 3 ) || !strncmp( buf->fw_id, "msadp", 5)) ) {
+                    dev_err(device,"firmware: look up FW device: %s, radio select = %s, skip path = %s\n", buf->fw_id, radio_image_info, fw_path[i]);
+                    continue;
+                  }
+                
 		file = filp_open(path, O_RDONLY, 0);
 		if (IS_ERR(file))
 			continue;
@@ -1135,6 +1164,12 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 
 	ret = 0;
 	timeout = firmware_loading_timeout();
+	
+	#if 1 
+	if (loading_timeout > 0 && !strncmp(desc->name, "msadp", 5))
+	timeout = 1 * HZ; 
+	#endif 
+		
 	if (opt_flags & FW_OPT_NOWAIT) {
 		timeout = usermodehelper_read_lock_wait(timeout);
 		if (!timeout) {
